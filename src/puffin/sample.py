6ac0024c30576c9fe4fe9873872793720f7e080f
ongzhixian <zhixian@hotmail.com>
2025-08-28 23:41:10 +0800

    Update continuous-integration.yml to deploy

---
+++ src/puffin/sample.py
@@ -0,0 +1,23 @@
+def sample_lambda1(event:dict, context):
+    """Example lambda function 2
+    Use case:
+        (example) This function is used to demonstrate how to handle an AWS Lambda event.
+    """
+
+    try:
+        print('Example Lambda function 1 triggered with event:', event)
+    except Exception as error:
+        print(error)
+        
+
+def sample_lambda1(event:dict, context):
+    """Sample lambda function 2
+    Use case:
+        (example) This function is used to demonstrate how to handle an AWS Lambda event.
+    """
+
+    try:
+        print('Example Lambda function 2 triggered with event:', event)
+    except Exception as error:
+        print(error)
+        
