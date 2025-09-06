8c427a556240cd8102d42ba9389fcf99a0863090
ongzhixian <zhixian@hotmail.com>
2025-09-06 18:46:29 +0800

    Add auth and user

---
+++ src/puffin/auth.py
@@ -0,0 +1,52 @@
+# Security related Lambdas
+# authenticate: Authenticates given credentials and return JWT
+# get_user_list: Get list of users.
+# add_user: Add user.
+# update_user_password: Update user password.
+# update_user_status: Update user status (activation/deactivation); consider specific accounts for each scenario.
+
+# Routes planning
+# POST  /authenticate   -- TODO_authenticate
+# GET   /user           -- TODO_get_user
+# POST  /user           -- TODO_add_user
+# PATCH /user/password  -- TODO_update_user_password
+# PATCH /user/status    -- TODO_update_user_status
+
+
+import json
+import logging
+# import os
+# import boto3
+from jwcrypto.jwk import JWK
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+# s3_client = boto3.client('s3')
+
+
+# ENDPOINT: /authenticate
+
+def authenticate(event:dict, context):
+    """
+    Authenticates given credentials and return JWT
+    
+    Args:
+        event (dict): The API Gateway event payload.
+        context (object): The Lambda context object.
+    
+    Returns:
+        dict: A response indicating success or failure.
+    """
+    try:
+        # Add your logic here
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'message': 'TODO: .'})
+        }
+    
+    except Exception as e:
+        logger.error(f"An error occurred: {e}", exc_info=True)
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'message': 'Internal Server Error', 'error': str(e)})
+        }
--- src/puffin/user.py
+++ src/puffin/user.py
@@ -5,16 +5,28 @@
 # update_user_password: Update user password.
 # update_user_status: Update user status (activation/deactivation); consider specific accounts for each scenario.
 
+# Routes planning
+# POST  /authenticate   -- TODO_authenticate
+# GET   /user           -- TODO_get_user
+# POST  /user           -- TODO_add_user
+# PATCH /user/password  -- TODO_update_user_password
+# PATCH /user/status    -- TODO_update_user_status
+
+
 import json
 import logging
 # import os
 # import boto3
+from jwcrypto.jwk import JWK
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 # s3_client = boto3.client('s3')
 
-def TODO_authenticate(event:dict, context):
+
+# ENDPOINT: /user
+
+def get_user_list(event:dict, context):
     """
     Authenticates given credentials and return JWT
     
@@ -39,7 +51,7 @@ def TODO_authenticate(event:dict, context):
             'body': json.dumps({'message': 'Internal Server Error', 'error': str(e)})
         }
     
-def TODO_get_user_list(event:dict, context):
+def add_user(event:dict, context):
     """
     Authenticates given credentials and return JWT
     
@@ -64,31 +76,9 @@ def TODO_get_user_list(event:dict, context):
             'body': json.dumps({'message': 'Internal Server Error', 'error': str(e)})
         }
     
-def TODO_add_user(event:dict, context):
-    """
-    Authenticates given credentials and return JWT
-    
-    Args:
-        event (dict): The API Gateway event payload.
-        context (object): The Lambda context object.
-    
-    Returns:
-        dict: A response indicating success or failure.
-    """
-    try:
-        # Add your logic here
-        return {
-            'statusCode': 200,
-            'body': json.dumps({'message': 'TODO: .'})
-        }
-    
-    except Exception as e:
-        logger.error(f"An error occurred: {e}", exc_info=True)
-        return {
-            'statusCode': 500,
-            'body': json.dumps({'message': 'Internal Server Error', 'error': str(e)})
-        }
-    
+
+# ENDPOINT: /user/password
+
 def TODO_update_user_password(event:dict, context):
     """
     Authenticates given credentials and return JWT
@@ -114,6 +104,9 @@ def TODO_update_user_password(event:dict, context):
             'body': json.dumps({'message': 'Internal Server Error', 'error': str(e)})
         }
     
+
+# ENDPOINT: /user/status
+
 def TODO_update_user_status(event:dict, context):
     """
     Authenticates given credentials and return JWT
